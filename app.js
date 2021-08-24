a3b145239d5becd86555b4f227e1212634ac2710
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-24 14:01:15 -0400

    part 14

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-new Vue({
-  el: '#vue-app',
+var one = new Vue({
+  el: '#vue-app-one',
   data: {
-    health: 100,
-    ended: false
+    title: 'Vue App one'
   },
   methods: {
-    punch: function() {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-    },
-    restart: function() {
-      this.health = 100;
-      this.ended = false;
+ 
+  },
+  computed: {
+    greet: function() {
+      return 'Hello from app one';
+    }
+  }
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue app two'
+  },
+  methods: {
+    changeTitle: function() {
+      one.title = 'Title changed'
     }
   },
   computed: {
-  
+    greet: function() {
+      return 'Hello from app tew';
+    } 
   }
 });
+
+two.title = 'changed from outside instance'
