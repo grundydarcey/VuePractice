b2516add7cc7da61dadf9e6f5f6af8517de0b530
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-24 14:13:58 -0400

    part 15

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-var one = new Vue({
-  el: '#vue-app-one',
-  data: {
-    title: 'Vue App one'
+var data = {
+    name: 'Yoshi'
+}
+
+Vue.component('greeting', {
+  template: '<p>Im a {{ name }}. <button v-on:click='changeName'>Change name</button> </p>',
+  data: function() {
+    return data;
   },
   methods: {
- 
-  },
-  computed: {
-    greet: function() {
-      return 'Hello from app one';
+    changeName: function() {
+      this.name = 'Mario';
     }
   }
 });
 
-var two = new Vue({
-  el: '#vue-app-two',
-  data: {
-    title: 'Vue app two'
-  },
-  methods: {
-    changeTitle: function() {
-      one.title = 'Title changed'
-    }
-  },
-  computed: {
-    greet: function() {
-      return 'Hello from app tew';
-    } 
-  }
+new Vue({
+  el: '#vue-app-one'
 });
 
-two.title = 'changed from outside instance'
+new Vue({
+  el: '#vue-app-two'
+});
