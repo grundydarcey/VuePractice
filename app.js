74ca199b119f2b76d3c1ae27b4000a7701be717d
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-23 23:34:35 -0400

    part three

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Darcey'
+    name: 'Darcey',
+    job: 'Software Application Developer'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ', ' + this.name;
+    }
   }
 });
